36e5c212a3eca8fc15a136747218f4cfc3f55496
YaoKevinn <vasty85817@gmail.com>
2021-12-11 03:16:38 -0300

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by YaoKevinn on 11/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
